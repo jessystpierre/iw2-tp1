52400b60225d5207bfc969d40f67bc85981c9768
Jessy St-Pierre <jessy.stpierre@live.com>
2023-05-27 19:48:35 -0400

    debut front-end header mobile

---
+++ js/mobile-menu.js
@@ -0,0 +1,15 @@
+const burger = document.querySelector('.burger-menu__trigger');
+const nav = document.querySelector('.menu');
+const closeMenu = document.querySelector('.close-menu');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+});
+
+
+closeMenu.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+});
+
